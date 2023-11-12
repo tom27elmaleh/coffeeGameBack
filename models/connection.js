0f3216046546c65422efb15e0f5569e2a042ccc4
Elmaleh Tom <tomelmaleh@hotmail.com>
2023-11-12 22:43:40 +0100

    final

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const connectionStr =
-  "mongodb+srv://admin:fRgonCXd8RxXs9Jr@cluster0.3pyhzah.mongodb.net/coffeeGame";
+const connectionStr = process.env.conString;
 
 mongoose
   .connect(connectionStr, { connectTimeoutMS: 2000 })
